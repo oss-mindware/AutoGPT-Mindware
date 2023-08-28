621ff700f96c37947630da4e51ae27946b14056e
Tomas Taylor <totaylor19@gmail.com>
2023-08-27 20:56:23 -0400

    Update plugin_function.py

--- src/mindware/plugin_function.py
+++ src/mindware/plugin_function.py
@@ -1,8 +1,11 @@
 class PluginFunction:
-    def __init__(self, url, name, path, method, description, response_type):
+    def __init__(self, url, name, path, params, method, auth_info, description, requires_auth, response_type):
         self.url = url
         self.name = name
         self.path = path
+        self.params = params
         self.method = method
+        self.auth_info = auth_info
         self.description = description
+        self.requires_auth = requires_auth
         self.response_type = response_type
